ec5820c980c20a5640d603ff139e10e385ca907e
Timothy Graupmann <tim.graupmann@razer.com>
2019-09-16 12:45:42 -0700

    Added fish spawn event

--- src/main/java/com/example/examplemod/MyForgeEventHandler.java
+++ src/main/java/com/example/examplemod/MyForgeEventHandler.java
@@ -476,6 +476,46 @@ public class MyForgeEventHandler extends ChromaEffects {
 
 	@SubscribeEvent
 	public void handleRightClickBlock(RightClickBlock event) {
+
+		if (event.getPlayer().getHeldItem(event.getHand()).getItem() == Items.PUFFERFISH){
+			System.out.println("Spawn puffer fish");
+			//avoid blocking the main thread
+			Timer timer = new Timer("Timer");
+			TimerTask task = new TimerTask() {
+				public void run() {
+					if (sChromaInitialized) {
+						showEffect17();
+						showEffect17ChromaLink();
+						showEffect17Headset();
+						showEffect17Mousepad();
+						showEffect17Mouse();
+					}
+				}
+			};
+			timer.schedule(task, 0);
+			return;
+		}
+
+		if (event.getPlayer().getHeldItem(event.getHand()).getItem() == Items.TROPICAL_FISH_SPAWN_EGG){
+			System.out.println("Spawn tropical fish");
+			//avoid blocking the main thread
+			Timer timer = new Timer("Timer");
+			TimerTask task = new TimerTask() {
+				public void run() {
+					if (sChromaInitialized) {
+						showEffect18();
+						showEffect18ChromaLink();
+						showEffect18Headset();
+						showEffect18Mousepad();
+						showEffect18Mouse();
+					}
+				}
+			};
+			timer.schedule(task, 0);
+			return;
+		}
+
+
 		String threadName = Thread.currentThread().getName();
 		switch (threadName) {
 		case "Server thread":
